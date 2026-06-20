1d19d242fcf6add9e3bf9eb3acf5f8cdc47bdd90
unknown <almenaresalfredo@gmail.com>
2026-06-20 11:58:16 -0400

    Fix GitIgnore 3

--- ADT/linkedlist.hs
+++
@@ -1,9 +0,0 @@
-data LinkedList a = 
-  Empty
-  | Node a (LinkedList a)   -- Node value near
-  deriving (Show)
-
--- List Traversal
-traverseList :: LinkedList a -> [a]
-traverseList Empty = [] 
-traverseList (Node a near) = [a] ++ traverseList near
